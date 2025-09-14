2e4b6942b23c53c9c27397268377857f6e5a4744
pvr600 <227826642+pvr600@users.noreply.github.com>
2025-09-14 07:41:13 +0200

    Add files via upload

--- api/log-scan.js
+++ api/log-scan.js
@@ -1,6 +1,1 @@
-
-export default async function handler(req, res) {
-  // Accepts scan pings; demo no-op
-  res.setHeader("content-type","application/json");
-  res.status(204).send("");
-}
+export default (req,res)=>res.status(204).send('');
